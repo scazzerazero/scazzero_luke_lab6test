438d1297715c5856581db35c3a5b980e5413580e
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-17 20:46:58 +0000

    why would I swap those?!

--- led_display.py
+++ led_display.py
@@ -21,9 +21,9 @@ class LEDdisplay():
     self.shifter = Shifter(data, latch, clock)
  
   def setNumber(self, num):  # display a given number
-    row=1 #change this value to pick which row the pattern appears on 
-    self.shifter.shiftByte(LEDdisplay.numbers[num])#load the row values
+    row = 1 #change this value to pick which row the pattern appears on 
     self.shifter.shiftByte(1<<(row-1)) #select the given row
+    self.shifter.shiftByte(LEDdisplay.numbers[num])#load the row values
     self.shifter.latch()
 
     '''To fix this for your lab code, my suggestion is to remove the latch step from shiftByte, and add a new method to Shifter called latch that will perform the latching function.  Then in your main code (or whatever code is calling shiftByte), call latch immediately after calling shiftByte twice.  This way you will only latch the registers after both bytes have been loaded.  This should eliminate strange flickering issues that some of you were seeing in class yesterday, and make your firefly output look much nicer in the lab.'''
